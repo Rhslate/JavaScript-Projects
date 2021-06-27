77ac822bebc94700a3de0c00637140cebaf79318
Rhslate <Rhslate@gmail.com>
2021-06-27 11:49:19 -0700

    Basic JavaScipt

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,24 @@
+var purple="The Slates.";  //Defining a function and naming it //Giving it a string value
+var purple = purple.fontcolor("purple");
+document.write(purple);  //Putting the value
+//of result into HTML element with "purple" id
+
+var Family="The Slates", Dad="Daniel", Mom="Susan", Daughter="Rachel", Son="Andrew";
+document.write(Mom);  //Defining a function and naming it //Giving it a string value
+
+var blues = "I have the blues.";
+var blues = blues.fontcolor("blue");
+document.write(blues);  //Putting the value
+//of result into HTML element with "blue" id
+
+document.write(10+10);  //Expression
+
+function My_First_Function() {     //Defining a function and naming it
+    var string = "This text is green!";   //Degining a variable and giving it a 
+                                        //string value
+
+    var result = str.fontcolor("green");  //Using the fontcolor method on
+                                         //str variable
+    document.getElementById("Green_Text") .innerHTML = result;  //Putting the value
+                                                        //of result into HTML element with "Green_Text" id
+}
