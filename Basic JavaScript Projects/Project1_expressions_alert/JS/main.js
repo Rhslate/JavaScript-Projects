816f0c0f0e55f36f96b3f7e2b5fbe77a22a103b6
Rhslate <Rhslate@gmail.com>
2021-06-27 11:59:01 -0700

    Basic JavaScript code updated

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -17,7 +17,7 @@ function My_First_Function() {     //Defining a function and naming it
     var string = "This text is green!";   //Degining a variable and giving it a 
                                         //string value
 
-    var result = str.fontcolor("green");  //Using the fontcolor method on
+    var result = string.fontcolor("green");  //Using the fontcolor method on
                                          //str variable
     document.getElementById("Green_Text") .innerHTML = result;  //Putting the value
                                                         //of result into HTML element with "Green_Text" id
