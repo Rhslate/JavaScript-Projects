2cc29ea81872bf2b43c79c24f089e2f2d7e2e670
Rhslate <Rhslate@gmail.com>
2021-07-01 08:10:45 -0700

    Basic JavaScript8

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -1,4 +1,4 @@
-function full_Sentence() {
+function full_Sentence() { //This method concatenates (connects) two or more strings
     var part_1 = "I have ";
     var part_2 = "made this ";
     var part_3 = "into a complete ";
@@ -7,18 +7,18 @@ function full_Sentence() {
     document.getElementById("Concatenate").innerHTML = whole_sentence;
 }
 
-function slice_Method() {
+function slice_Method() { //A string method that extracts a section of a string and then returns the extracted section in a new string
     var Sentence = "All work and no play makes Johnny a dull boy."; //I appreciate this so much! Hahaha! I love Horror movies
     var Section = Sentence.slice(27,33);
     document.getElementById("Slice").innerHTML = Section;
 }
 
-function string_Method() {
+function string_Method() { //Assists with working with numbers 
     var X =182;
     document.getElementById("Numbers_to_string").innerHTML = X.toString();
 }
 
-function precision_Method() {
+function precision_Method() { //This formats a number to a specified lenght
     var X = 12938.3012987376112;
     document.getElementById("Precision").innerHTML = X.toPrecision(10);
 }
